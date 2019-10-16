11569065736f97b0337bcde07adeb64a7ae969cf
Lucas Gonçalves <lucas@strongtecnologia.com.br>
2019-10-16 15:46:44 -0300

    primeiro commit

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,63 +1,65 @@
-import React, {useState, useEffect, Component} from 'react';
-import {
-  Text,
-  Image,
-  StyleSheet,
-  Dimensions,
-  View,
-  StatusBar,
-  TouchableOpacity,
-  SafeAreaView
-} from 'react-native';
-import {Drawer} from 'react-native-paper';
-import {DrawerActions} from 'react-navigation';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import * as React from "react";
+import { TouchableOpacity, View, Image, SafeAreaView } from "react-native";
+import { Appbar, Avatar, withTheme, DefaultTheme } from "react-native-paper";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import { DrawerActions } from "react-navigation-drawer";
 
-import {
-  Container,
-  Content,
-  Title,
-  Form,
-  Input,
-  Submit,
-  List,
-  TextLight,
-  DrawerItem,
-  TextDark,
-  Link,
-  Send,
-  SubTitle,
-} from './styles';
+class HeaderApp extends React.Component {
+  componentWillMount() {
+    //alert(JSON.stringify(this.props.navigation));
+  }
 
-import logo from '~/assets/logo.png';
+  _goMenu = () => this.props.navigation.dispatch(DrawerActions.toggleDrawer());
 
-export default function ToolBar(props) {
-  useEffect(() => {
-    alert(JSON.stringify(props));
-  }, []);
+  render() {
 
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <StatusBar backgroundColor="transparent" barStyle="light-content" />
+    return (
+      <SafeAreaView style={{flex: 1}}>
 
-      <View
-        style={{
-          flexDirection: 'row',
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }}>
-        <View style={{width: 50, alignItems: 'center'}}>
+        <View
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            height: 50,
+            flexDirection: "row",
+            elevation: 4
+          }}
+        >
           <TouchableOpacity
-            onPress={() =>
-                props.navigation.navigate('DrawerOpen')
-            }>
-            <MaterialCommunityIcons name="menu" size={24} color={'#fff'} />
+            onPress={this._goMenu}
+            style={{
+              width: 60,
+              height: 50,
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <Appbar.Action
+              icon="menu"
+              onPress={this._goMenu}
+              color={'#fff'}
+            />
           </TouchableOpacity>
+
+          <View
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <Image
+              resizeMode="contain"
+              style={{ height: 35, maxWidth: 150 }}
+              source={require("../assets/logo.png")}
+            />
+          </View>
+
+          <View style={{ width: 60 }} />
         </View>
-        <Image style={{width: 70, height: 35}} source={logo} />
-        <View style={{width: 50}}></View>
-      </View>
-    </SafeAreaView>
-  );
+      </SafeAreaView>
+    );
+  }
 }
+
+export default HeaderApp;
